06f7bf83362311688c6cde6e748e9a7d7faf6c72
CoptimT <zxw644@163.com>
2016-01-07 19:29:19 +0800

    flume has two type of source

---
+++ Flume/flume-source/src/main/java/cn/zxw/bigdata/flume/source/MySourceType1.java
@@ -0,0 +1,54 @@
+package cn.zxw.bigdata.flume.source;
+
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.EventDeliveryException;
+import org.apache.flume.PollableSource;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.source.AbstractSource;
+
+public class MySourceType1 extends AbstractSource implements Configurable,PollableSource {
+	private String myProp;
+	
+	public void configure(Context context) {
+		String myProp = context.getString("myProp", "defaultValue");
+		// Process the myProp value (e.g. validation, convert to another type,
+		// ...)
+		// Store myProp for later retrieval by process() method
+		this.myProp = myProp;
+	}
+
+	@Override
+	public void start() {
+		// Initialize the connection to the external client
+	}
+
+	@Override
+	public void stop() {
+		// Disconnect from external client and do any additional cleanup
+		// (e.g. releasing resources or nulling-out field values) ..
+	}
+	
+	public Status process() throws EventDeliveryException {
+		Status status = null;
+		try {
+			// This try clause includes whatever Channel/Event operations you want to do
+			// Receive new data
+			Event e = null;// getSomeData();
+			// Store the Event into this Source's associated Channel(s)
+			getChannelProcessor().processEvent(e);
+			status = Status.READY;
+		} catch (Throwable t) {
+			// Log exception, handle individual exceptions as needed
+			status = Status.BACKOFF;
+			// re-throw all Errors
+			if (t instanceof Error) {
+				throw (Error) t;
+			}
+		} finally {
+			// txn.close();
+		}
+		return status;
+	}
+
+}
---
+++ Flume/flume-source/src/main/java/cn/zxw/bigdata/flume/source/MySourceType2.java
@@ -0,0 +1,16 @@
+package cn.zxw.bigdata.flume.source;
+
+import org.apache.flume.Context;
+import org.apache.flume.EventDrivenSource;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.source.AbstractSource;
+
+public class MySourceType2 extends AbstractSource implements Configurable,EventDrivenSource {
+
+	public void configure(Context context) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}

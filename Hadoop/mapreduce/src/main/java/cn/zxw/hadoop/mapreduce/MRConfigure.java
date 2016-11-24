976e49af89deca00a11513d35b17c835740bfb69
CoptimT <zxw644@163.com>
2016-11-24 20:14:53 +0800

    add hadoop code

--- Hadoop/mapreduce/src/main/java/cn/zxw/hadoop/mapreduce/MRConfigure.java
+++ Hadoop/mapreduce/src/main/java/cn/zxw/hadoop/mapreduce/MRConfigure.java
@@ -20,10 +20,14 @@ public class MRConfigure extends Configured implements Tool {
 		//map输入数据最小切分(老版本配置mapred.min.split.size)
 		conf.set("mapreduce.input.fileinputformat.split.minsize", "536870912");//512M
 		//设置map任务数(老版本配置mapred.map.tasks)
-		conf.set("mapreduce.job.maps", "1");//如DB导出的job
-		//设置map端压缩
+		conf.set("mapreduce.job.maps", "1");//如DB导出HDFS的job
+		//设置map端压缩(老版本配置mapred.compress.map.output)
 		conf.set("mapreduce.map.output.compress", "true");
-				
+		//推测执行(老版本配置mapred.map.tasks.speculative.execution)
+		//If true, then multiple instances of some map tasks may be executed in parallel.
+		conf.setBoolean("mapred.map.tasks.speculative.execution", false);
+		
+		
 		Job job = Job.getInstance(conf, MRConfigure.class.getSimpleName());
 		job.setJarByClass(MRConfigure.class);
 		//...
